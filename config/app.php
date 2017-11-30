37c018cb489db871c92f0b2bf5e6762b7a929fa0
KEEL <KEEL@KEELs-MacBook-Pro.local>
2017-11-30 23:34:36 +0900

    some test commit;

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-    'salt' => env('SECURITY_SALT', '__SALT__'),
+    'salt' => env('SECURITY_SALT', 'fsjdkfjsoiurow$#!@#@#$%%^#@!'),
     ],
     
     /**
