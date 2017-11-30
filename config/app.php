f03c8b399295a631c2af09f1af42d8df7253655b
KEEL <KEEL@KEELs-MacBook-Pro.local>
2017-11-30 20:27:17 +0900

    some test commit.

--- config/app.php
+++ config/app.php
@@ -222,13 +222,13 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => '127.0.0.1',
+            'host' => 'localhost',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
-            'port' => '3306',
+            //'port' => '3306',
             'username' => getenv("MYSQL_USER"),
             'password' => getenv("MYSQL_PWD"),
             'database' => getenv("MYSQL_DATABASE"),
