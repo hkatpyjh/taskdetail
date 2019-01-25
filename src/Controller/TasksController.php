9f344c6d3b18d74712716b782641f3fc76d02f8c
hkatpyjh <ksd523288@icloud.com>
2019-01-25 20:20:49 +0900

    Update TasksController.php

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -106,7 +106,8 @@ class TasksController extends AppController
         
         $tasks_json = json_decode(json_encode($tasks), Tasks::class);
 
-        $json = json_decode($this->request->getData('json'), Tasks::class);
+        $json = json_decode($this->request->getData('json'), true);
+        debug($json);
 
         $results = Hash::merge($tasks_json, $json);
 
